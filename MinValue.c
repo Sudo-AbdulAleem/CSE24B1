bbc6bff066c7616e7d277e0cee2f7d21e55e8c5f
Sudo-AbdulAleem <theahmed1771@gmail.com>
2024-10-29 17:01:27 +0530

    Re-written the code

--- MinValue.c
+++ MinValue.c
@@ -1,14 +1,26 @@
 #include<stdio.h>
 int main(){
-int a[5];
+int len;
+printf("Enter The Length Of Array: ");
+scanf("%d",&len);
+int a[len];
 int i;
-//for(i=0;i<5;++i)
-scanf("%d",a);
 
+for(i=0;i<len;++i){
+printf("Enter a[%d]:",i);
+scanf("%d",&a[i]);
+}
+printf("\n");
+int minval = a[0];
+for (i=0;i<len;++i)
+if(a[i]<minval)
+minval=a[i];
+printf("Minimum Value in the array is %d\n",minval);
+printf("\n\n");
 /*
-for(i=0;i<5;++i)
+printf("The given array was\n");
+for(i=0;i<10;++i)
 printf("%d\n",a[i]);
 */
-printf("%
 return 0;
 }
